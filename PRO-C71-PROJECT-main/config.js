13fb8e1e3d1b68d57b3b39e67c0ee9311b9e2f84
yuvaanparwaney <63131496+yuvaanparwaney@users.noreply.github.com>
2021-05-15 13:26:54 -0700

    Update config.js

--- PRO-C71-PROJECT-main/config.js
+++ PRO-C71-PROJECT-main/config.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+const firebaseConfig = {
+  apiKey: "AIzaSyB808IYyNdYizio-fNszlxSmoWUz4-dlvo",
+  authDomain: "story-hub-p70-or-p71.firebaseapp.com",
+  databaseURL: "https://story-hub-p70-or-p71-default-rtdb.firebaseio.com",
+  projectId: "story-hub-p70-or-p71",
+  storageBucket: "story-hub-p70-or-p71.appspot.com",
+  messagingSenderId: "646128645310",
+  appId: "1:646128645310:web:2da750b8250d7d84125aef"
 };
 
 firebase.initializeApp(firebaseConfig);
